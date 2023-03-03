c817d999628f9a41aaad7bd78bcfd1e4e12655d7
EricLam404 <lameric9888@gmail.com>
2023-03-03 13:34:08 -0500

    changed to use express.js

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const fs = require('fs').promises;
 
-const server = http.createServer(async (req, res) => {
-  try {
-    let content;
-    switch (req.url) {
-      case '/':
-        content = await fs.readFile('index.html', 'utf-8');
-        break;
-      case '/about':
-        content = await fs.readFile('about.html', 'utf-8');
-        break;
-      case '/contact-me':
-        content = await fs.readFile('contact-me.html', 'utf-8');
-        break;
-      default:
-        content = await fs.readFile('404.html', 'utf-8');
-        break;
-    }
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(content);
-    res.end();
-  } catch (err) {
-    console.error(err);
-    res.writeHead(500, { 'Content-Type': 'text/plain' });
-    res.write('500 Internal Server Error');
-    res.end();
+app.use(async (req, res) => {
+  let url = req.url;
+
+  if(url == "/"){
+    const content = await fs.readFile('index.html', 'utf-8');
+    res.send(content);
   }
+  else if(url == "/about"){
+    const content = await fs.readFile('about.html', 'utf-8');
+    res.send(content);
+  }
+  else if(url == "/contact-me"){
+    const content = await fs.readFile('contact-me.html', 'utf-8');
+    res.send(content);
+  }
+  else{
+    const content = await fs.readFile('404.html', 'utf-8');
+    res.send(content);
+  }
+  
 });
 
-server.listen(8080, () => {
-  console.log('Listening on port 8080')
+const port = 8080;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
